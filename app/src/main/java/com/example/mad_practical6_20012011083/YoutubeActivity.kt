dc83aad8a57480628b85adfb2633fa4d21756b77
Dev <devkumarpatel20@gnu.ac.in>
2022-09-21 17:43:53 +0530

    Practical 6

--- app/src/main/java/com/example/mad_practical6_20012011083/YoutubeActivity.kt
+++ app/src/main/java/com/example/mad_practical6_20012011083/YoutubeActivity.kt
@@ -1,10 +1,12 @@
 package com.example.mad_practical6_20012011083
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebSettings
 import android.webkit.WebView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class YoutubeActivity : AppCompatActivity() {
     private val youtubeId="kRZ9_G7M3TQ"
@@ -13,12 +15,19 @@ class YoutubeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_youtube)
 
+        val mywebview = findViewById<WebView>(R.id.webView)
+        val myactionbutton = findViewById<FloatingActionButton>(R.id.btn2)
 
-        val myWebView = findViewById<WebView>(R.id.webView)
-        val settings = myWebView.settings
-        settings.javaScriptEnabled = true
-        settings.loadWithOverviewMode = true
-        settings.useWideViewPort=true
-        myWebView.loadUrl("https://www.youtube.com/embed/$youtubeId")
+        myactionbutton.setOnClickListener(){
+            Intent(this,MainActivity::class.java).apply { startActivity(this) }
+        }
+
+        val youtubeid = "97JGkxPROOY"
+
+        val webSettings:WebSettings = mywebview.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.loadWithOverviewMode = true
+        webSettings.useWideViewPort = true
+        mywebview.loadUrl("https://www.youtube.com/embed/$youtubeid")
     }
 }
